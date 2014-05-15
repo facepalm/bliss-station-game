6c9bf6829062e940ca4ebbb3dc4cda0cf83e1aff
Brad Busse <bbusse@gmail.com>
2014-05-15 09:25:21 -0400

    Smooths docking procedure

--- equipment_computer.py
+++ equipment_computer.py
@@ -76,7 +76,7 @@ class DockingPath():
         self.end_orient = ( mod_dock.orientation + mod_dock.equipment[dock_dock][1] - mod_docker.equipment[dock_docker][1] ) + np.array([math.pi, 0])
         self.end_orient %= 2*math.pi
         
-        loc_offset = absolute_xyz(np.array([0,0,0]), mod_docker.equipment[dock_docker][0], mod_docker.orientation, mod_docker.size)
+        loc_offset = absolute_xyz(np.array([0,0,0]), mod_docker.equipment[dock_docker][0], self.end_orient, mod_docker.size)
         
         self.end_coords = mod_dock.getXYZ(mod_dock.equipment[dock_dock][0]) - loc_offset
         
@@ -85,14 +85,15 @@ class DockingPath():
         self.x = np.array( [0, duration/4, 3*duration/4, duration] )
         x=self.x
         w=np.array([1,10,10,5])
-        self.c0 = UnivariateSpline(x, np.array([self.start_coords[0], self.start_coords[0]+10*math.cos(self.start_orient[0]), self.end_coords[0]-30*math.sin(self.end_orient[0]), self.end_coords[0]]), w=w,k=1, s=0)
-        self.c1 = UnivariateSpline(x, np.array([self.start_coords[1], self.start_coords[1]+10*math.sin(self.start_orient[1]), self.end_coords[1]-30*math.cos(self.end_orient[1]), self.end_coords[1]]), w=w,k=1, s=0)
-        self.c2 = UnivariateSpline(x, np.array([self.start_coords[2], self.start_coords[2], self.end_coords[2], self.end_coords[2]]), w=w,k=1, s=0)
+        self.c0 = UnivariateSpline(x, np.array([self.start_coords[0], self.start_coords[0]+10*math.cos(self.start_orient[0]), self.end_coords[0]-30*math.cos(self.end_orient[0]), self.end_coords[0]]), w=w,k=2, s=0)
+        self.c1 = UnivariateSpline(x, np.array([self.start_coords[1], self.start_coords[1]+10*math.sin(self.start_orient[0]), self.end_coords[1]-30*math.sin(self.end_orient[0]), self.end_coords[1]]), w=w,k=2, s=0)
+        self.c2 = UnivariateSpline(x, np.array([self.start_coords[2], self.start_coords[2], self.end_coords[2], self.end_coords[2]]), w=w,k=2, s=0)
             
-        self.o0 = UnivariateSpline(x, np.array([self.start_orient[0], self.start_orient[0], self.end_orient[0], self.end_orient[0]]), w=w,k=1, s=0)
-        self.o1 = UnivariateSpline(x, np.array([0,0,0,0]), w=w,k=1, s=0)
+        self.o0 = UnivariateSpline(x, np.array([self.start_orient[0], self.start_orient[0], self.end_orient[0], self.end_orient[0]]), w=w,k=2, s=0)
+        self.o1 = UnivariateSpline(x, np.array([0,0,0,0]), w=w,k=2, s=0)
         
     def get_time_point(self,t=0):
+        #return [ self.end_coords, self.end_orient ]
         #print [np.array([self.c0(t), self.c1(t), self.c2(t)]), np.array([self.o0(t), self.o1(t)]) ]
         return [np.array([self.c0(t), self.c1(t), self.c2(t)]), np.array([self.o0(t), self.o1(t)]) ]
         
