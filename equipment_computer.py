d2e9ff0e6695e8baed814b45ea5aa7dcd1bb0384
Brad Busse <bbusse@gmail.com>
2014-05-14 23:45:21 -0400

    Docking animation kind of in, but still glitchy

--- equipment_computer.py
+++ equipment_computer.py
@@ -5,8 +5,10 @@ import util
 import atmospherics
 from tasks import Task
 from filtering import Searcher
+import numpy as np
 from scipy.interpolate import UnivariateSpline
-import random
+import random, math
+from generic_module import absolute_xyz
      
 class Computer(Equipment):
     '''Progenitor class for non-moving computery bits'''
@@ -46,6 +48,8 @@ class DockingComputer(Computer, Rack):
         if self.docking_item[0] and not self.docking_item[1]: 
             self.docking_item[1] = self.docking_item[0].get_random_dock(side_port_allowed=False)                                        
         
+        self.docking_path = DockingPath(self.docking_target[0], self.docking_target[1], self.docking_item[0], self.docking_item[1],self.docking_duration)
+        
         self.docking_task = Task("Dock module", owner = self, timeout=None, task_duration = self.docking_duration, severity='MODERATE', fetch_location_method=Searcher(self,self.installed.station).search,logger=self.logger)
         self.installed.station.tasks.add_task(self.docking_task)    
 
@@ -53,11 +57,43 @@ class DockingComputer(Computer, Rack):
         if not task or not self.installed: return
         if task.name == "Dock module":
             self.installed.station.berth_module(self.docking_target[0], self.docking_target[1], self.docking_item[0], self.docking_item[1])        
+            
+    def task_work_report(self,task,dt):
+        if task.name.startswith('Dock module'):
+            self.docking_item[0].location, self.docking_item[0].orientation = self.docking_path.get_time_point(task.task_duration - task.task_duration_remaining)
+            self.docking_item[0].refresh_image()
+            
                 
             
 class DockingPath():
-    def __init__(self, start_coords, start_orient, end_coords, end_orient, duration):
-        pass
+    def __init__(self, mod_dock, dock_dock, mod_docker, dock_docker, duration):
+        self.duration=duration
+        #start_coords, start_orient, end_coords, end_orient, duration):
+        self.start_coords = mod_docker.location
+        self.start_orient = mod_docker.orientation
+        self.start_orient %= 2*math.pi
+        
+        self.end_orient = ( mod_dock.orientation + mod_dock.equipment[dock_dock][1] - mod_docker.equipment[dock_docker][1] ) + np.array([math.pi, 0])
+        self.end_orient %= 2*math.pi
+        
+        loc_offset = absolute_xyz(np.array([0,0,0]), mod_docker.equipment[dock_docker][0], mod_docker.orientation, mod_docker.size)
+        
+        self.end_coords = mod_dock.getXYZ(mod_dock.equipment[dock_dock][0]) - loc_offset
+        
+        #print self.start_coords, self.end_coords, self.start_orient, self.end_orient        
+        
+        self.x = np.array( [0, duration/4, 3*duration/4, duration] )
+        x=self.x
+        w=np.array([1,10,10,5])
+        self.c0 = UnivariateSpline(x, np.array([self.start_coords[0], self.start_coords[0]+10*math.cos(self.start_orient[0]), self.end_coords[0]-30*math.sin(self.end_orient[0]), self.end_coords[0]]), w=w,k=1, s=0)
+        self.c1 = UnivariateSpline(x, np.array([self.start_coords[1], self.start_coords[1]+10*math.sin(self.start_orient[1]), self.end_coords[1]-30*math.cos(self.end_orient[1]), self.end_coords[1]]), w=w,k=1, s=0)
+        self.c2 = UnivariateSpline(x, np.array([self.start_coords[2], self.start_coords[2], self.end_coords[2], self.end_coords[2]]), w=w,k=1, s=0)
             
+        self.o0 = UnivariateSpline(x, np.array([self.start_orient[0], self.start_orient[0], self.end_orient[0], self.end_orient[0]]), w=w,k=1, s=0)
+        self.o1 = UnivariateSpline(x, np.array([0,0,0,0]), w=w,k=1, s=0)
+        
+    def get_time_point(self,t=0):
+        #print [np.array([self.c0(t), self.c1(t), self.c2(t)]), np.array([self.o0(t), self.o1(t)]) ]
+        return [np.array([self.c0(t), self.c1(t), self.c2(t)]), np.array([self.o0(t), self.o1(t)]) ]
         
 util.equipment_targets['Docking Computer'] = DockingComputer
